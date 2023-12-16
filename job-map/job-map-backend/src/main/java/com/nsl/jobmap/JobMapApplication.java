0f6eb4c1bdc40aa9fb3c4372f1dc503aefaee1ae
pgd <rudeh1253@gmail.com>
2023-12-17 03:25:00 +0900

    update .gitignore

---
+++ job-map/job-map-backend/src/main/java/com/nsl/jobmap/JobMapApplication.java
@@ -0,0 +1,13 @@
+package com.nsl.jobmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMapApplication.class, args);
+	}
+
+}
---
+++ job-map/job-map-backend/src/test/java/com/nsl/jobmap/JobMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsl.jobmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
