f2a566ceebd5c3e40f47876a79ceaa65bc4d6ec7
pgd <rudeh1253@gmail.com>
2024-01-26 17:23:47 +0900

    feat: add an class for container object

---
+++ term-list/src/main/java/personal/nsl/container/container/Container.java
@@ -0,0 +1,30 @@
+package personal.nsl.container.container;
+
+public final class Container {
+    private long code;
+    private String name;
+    
+    public Container() {
+    }
+
+    public Container(long code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
